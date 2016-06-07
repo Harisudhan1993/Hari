936f361faf9d58591b3b5558f63a97f6cfc998ac
Harisudhan1993 <harisudhan11051993@gmail.com>
2016-06-07 23:40:12 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-Public class Hello { 
-   Public static void main(String[] rgs) { 
+public class Hello { 
+   public static void main(String[] args) { 
       System.out.println("Hello, World");
    }
 }
